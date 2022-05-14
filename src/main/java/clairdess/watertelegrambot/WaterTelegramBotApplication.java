a879829a0ed36bdd28741aad8fbbb7a66982b763
Daniil Davydov <clairdess.github@gmail.com>
2022-05-15 06:23:48 +0900

    Initial commit

---
+++ src/main/java/clairdess/watertelegrambot/WaterTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package clairdess.watertelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/clairdess/watertelegrambot/WaterTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package clairdess.watertelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
